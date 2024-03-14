49618cb6b71a29da1b79bcac7d151b2f0269e4d0
Daniele206 <danieledeieso2004@gmail.com>
2024-03-14 13:56:25 +0100

    [add] Milestone1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,40 @@ createApp({
 
     return{
       darkMode: false,
+      listChat:[
+        {
+          name: 'Michele',
+          img: './assets/img/avatar_1.jpg'
+        },
+        {
+          name: 'Fabio',
+          img: './assets/img/avatar_2.jpg'
+        },
+        {
+          name: 'Samuele',
+          img: './assets/img/avatar_3.jpg'
+        },
+        {
+          name: 'Alessandro B.',
+          img: './assets/img/avatar_4.jpg'
+        },
+        {
+          name: 'Alessandro L.',
+          img: './assets/img/avatar_5.jpg'
+        },
+        {
+          name: 'Claudia',
+          img: './assets/img/avatar_6.jpg'
+        },
+        {
+          name: 'Federico',
+          img: './assets/img/avatar_7.jpg'
+        },
+        {
+          name: 'Davide',
+          img: './assets/img/avatar_8.jpg'
+        },
+      ]
     }
 
   }
